b18b74ca973ec0cf5616ce812cb3ec91dfa4def5
k-akie <kikuchiakie7@gmail.com>
2022-03-05 21:31:43 +0900

    緊急事態宣言などを背景色で表す

--- make_output.py
+++ make_output.py
@@ -31,12 +31,36 @@ def data_merge(target_columns, df_population, df_infected):
     return df_result.T
 
 
+# 緊急事態宣言等の履歴
+# https://www.kwm.co.jp/blog/state-of-emergency/
+emergency_term = [
+    {'2020/4/7~2020/5/21', '緊急事態宣言'},  # 1回目
+    {'2021/1/14~2021/2/28', '緊急事態宣言'},  # 2回目
+    {'2021/4/25~2021/6/20', '緊急事態宣言'},  # 3回目、2回目のまん防に変わった
+    {'2021/8/2~2021/9/30', '緊急事態宣言'},  # 4回目
+    {'2021/4/5~2021/4/24', 'まん延防止等重点措置'},  # 1回目、3回目の緊急事態宣言に変わった
+    {'2021/6/21~2021/8/1', 'まん延防止等重点措置'},  # 2回目、4回目の緊急事態宣言に変わった
+    {'2022/1/27~2022/3/21', 'まん延防止等重点措置'},  # 3回目、延長中
+]
+
+
 def make_graph(df_result, prefectures, gender):
     # グラフ全体の設定
-    plt.figure(figsize=(12.0, 8.0))  # 横、縦
+    plt.figure(figsize=(15.0, 8.0))  # 横、縦
     plt.plot(df_result)
     plt.legend(df_result.columns)
     plt.title(f"{prefectures} [{gender}]", fontsize=14)
+
+    # 背景 https://bunsekikobako.com/axvspan-and-axhspan/
+    # 後ろは次の期間を指定する
+    # 緊急事態宣言
+    plt.axvspan('2021/1/6~2021/1/12', '2021/2/17~2021/2/23', color="orange", alpha=0.3)
+    plt.axvspan('2021/4/21~2021/4/27', '2021/6/23~2021/6/29', color="orange", alpha=0.3)
+    plt.axvspan('2021/8/4~2021/8/10', '2021/10/6~2021/10/12', color="orange", alpha=0.3)
+    # まん防
+    plt.axvspan('2021/4/7~2021/4/13', '2021/4/21~2021/4/27', color="yellow", alpha=0.3)
+    plt.axvspan('2021/6/23~2021/6/29', '2021/8/4~2021/8/10', color="yellow", alpha=0.3)
+    plt.axvspan('2022/1/26~2022/2/1', '2022/2/23~2022/3/1', color="yellow", alpha=0.3)
 
     # Y軸
     plt.ylabel('infected persons per population(%)')
