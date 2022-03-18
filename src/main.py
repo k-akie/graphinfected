042539132d10f8b89c655ebb50e1e870ecd26e2f
k-akie <kikuchiakie7@gmail.com>
2022-03-18 18:09:05 +0900

    処理を整理した

--- src/main.py
+++ src/main.py
@@ -13,25 +13,21 @@ from type.prefecture.Prefecture import Prefecture
 
 if __name__ == '__main__':
     osaka = Prefecture(
-        PrefName('osaka', '大阪')
-        , PrefCode('.27')
+        PrefName('osaka', '大阪'),
+        PrefCode('.27')
     )
 
+    # 入力
     # 人口データ
     population = read_population_osaka_fu(list(generation_dict.keys()), FilePath.input('jinkou-xlslist.csv'), 'UTF-8')
-
     # 感染者数データ
     infected = read_infected(FilePath.input('newly_confirmed_cases_detail_weekly.csv'), 'UTF-8', osaka.code)
 
+    # 出力
     # グラフに日本語を使う設定
     matplotlib.rc('font', family='BIZ UDGothic')
-
-    # 出力(割合)
-    make_output_ratio(generation_dict, population, infected, osaka.name, Grouping.MALE)
-    make_output_ratio(generation_dict, population, infected, osaka.name, Grouping.FEMALE)
-    make_output_ratio(generation_dict, population, infected, osaka.name, Grouping.ALL)
-
-    # 出力(生値)
-    make_output_row(generation_dict, population, infected, osaka.name, Grouping.MALE)
-    make_output_row(generation_dict, population, infected, osaka.name, Grouping.FEMALE)
-    make_output_row(generation_dict, population, infected, osaka.name, Grouping.ALL)
+    for group in Grouping:
+        # 割合
+        make_output_ratio(generation_dict, population, infected, osaka.name, group)
+        # 生値
+        make_output_row(generation_dict, population, infected, osaka.name, group)
